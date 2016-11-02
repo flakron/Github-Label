afbc3e21822323545d56998f3421e492ab2713ae
Flakron Bytyqi <flakron@gmail.com>
2016-11-02 11:52:29 +0100

    initial commit

---
+++ src/main/java/net/flakron/github/label/GithubLabelApplication.java
@@ -0,0 +1,12 @@
+package net.flakron.github.label;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubLabelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubLabelApplication.class, args);
+    }
+}
