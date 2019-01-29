8f15492971ef9bc2586e0efe8c5bc08f97594825
Flakron Bytyqi <flakron@gmail.com>
2019-01-29 11:03:57 +0100

    enable logging

--- src/main/java/net/flakron/github/label/services/impl/HipchatServiceImpl.java
+++ src/main/java/net/flakron/github/label/services/impl/HipchatServiceImpl.java
@@ -7,8 +7,12 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.logging.Logger;
+
 @Service
 public class HipchatServiceImpl implements HipchatService {
+
+    private final Logger logger = Logger.getLogger(HipchatServiceImpl.class.getName());
 
     @Value("${hipchat.url}")
     private String hipchatUrl;
@@ -17,6 +21,9 @@ public class HipchatServiceImpl implements HipchatService {
     public void notify(Notification notification) {
         if (notification != null) {
             RestTemplate rest = new RestTemplate();
+
+            logger.info("Got notification");
+
             rest.postForEntity(hipchatUrl, notification, Notification.class);
         }
     }
